61672f199fd6d4923e4eb8185c80cc9bdae9ca33
Lucas <lucasakajiri@hotmail.com>
2021-11-04 10:29:06 -0300

    fim da parte 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React from 'react';
+import Layout from './components/layout';
 
 function App() {
-  return (
-    <div className="App">
-      github api
-      <h1>bom dia</h1>
-    </div>
-  );
+  return <main>
+    <Layout>
+      <div>User
+        <img source="https://avatars.githubusercontent.com/u/73747647?v=4" alt="picture of user" />
+        <h1>nome</h1>
+        <h3>Username: </h3>
+        <span></span>
+        <div>
+          <h4>Followers</h4>
+          <span>0</span>
+        </div>
+        <div>
+          <h4>Starreds</h4>
+          <span>0</span>
+        </div>
+        <div>
+          <h4>Followings</h4>
+          <span>0</span>
+        </div>
+          
+
+      
+      </div>
+      <div>Repositories</div>
+      <div>Starreds</div>
+    </Layout>
+  </main>
 }
 
 export default App;
